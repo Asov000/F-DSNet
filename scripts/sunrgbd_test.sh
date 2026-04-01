15b26d9ff91abb7047451ee122b37f7c5bc3f3f6
Asov000 <166956370+Asov000@users.noreply.github.com>
2026-04-01 19:50:39 +0800

    Add files via upload

---
+++ scripts/sunrgbd_test.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+set -x
+set -e
+
+export CUDA_VISIBLE_DEVICES=0
+OUTDIR='output/sunrgbd'
+MODEL_FILE='models/det_base_sunrgbd.py'
+CFG_FILE='cfgs/det_sample_sunrgbd.yaml'
+
+# python sunrgbd/prepare_data.py  --gen_val_rgb_detection
+python train/test_net_det_sunrgbd.py --cfg $CFG_FILE OUTPUT_DIR $OUTDIR TEST.WEIGHTS $OUTDIR/model_best.pth SAVE_SUB_DIR test FROM_RGB_DET True
